bfa162022e40ecfc5e9051add10c44c645c3a48a
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-09 14:35:02 +0530

    Update looping.go
    Looping the site links

--- looping.go
+++ looping.go
@@ -1,21 +1,35 @@
+
 package main
 
-import "fmt"
+import ("fmt"
+"net/http"
+"io"
+"encoding/xml"
+)
 
+type SitemapIndex struct {
+	Locations []Location `xml:"url"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (L Location) String() string {
+	return fmt.Sprintf(L.Loc)
+}
 
 func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-world-sitemap.xml")
+	bytes, _ := io.ReadAll(resp.Body)
+	resp.Body.Close()
 
-	for x:=1; x<10; x++{
-		fmt.Println(x)
-	}
-
-	y:=5
-
-	for{
-		fmt.Println("Just do it",y)
-		y += 5
-		if y>25 {
-			break
-		}
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	//fmt.Println(s.Locations)
+	
+	for _, Loc := range s.Locations {
+		fmt.Printf("\n%s",Loc)
 	}
 }
+
