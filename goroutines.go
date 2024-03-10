c58299412105eddf80401a88804e56217b6921f7
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-10 18:54:33 +0530

    Adding defer

--- goroutines.go
+++ goroutines.go
@@ -9,15 +9,17 @@ import (
 var wg sync.WaitGroup
 
 func say(s string) {
+	
+	defer wg.Done()
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond*100)
 	}
-	wg.Done()
+	
 }
 
 func main() {
-	
+
 	//First collection of goroutines
 	wg.Add(1)
 	go say("Hi")
