ac273584b6a1d1f94971ac062465a90987137587
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-11 10:45:53 +0530

    Panic and Recover

--- goroutines.go
+++ goroutines.go
@@ -8,14 +8,24 @@ import (
 
 var wg sync.WaitGroup
 
-func say(s string) {
-	
+func cleanup() {
 	defer wg.Done()
-	for i := 0; i < 3; i++ {
+	defer fmt.Println("DONE")
+	if r := recover(); r != nil {
+		fmt.Println("Recovered in cleanup : ",r)
+	}
+}
+
+func say(s string) {
+	//defer wg.Done()
+	defer cleanup()
+	for i := 0; i < 5; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond*100)
-	}
-	
+		if i == 3 {
+			panic("Stop ... SOS")
+		}
+	}	
 }
 
 func main() {
