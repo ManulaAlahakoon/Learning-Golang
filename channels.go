058f08f7e94b9e3459171b55b0144054b10739b8
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-11 12:56:49 +0530

    Buffering and iterationg over channels

--- channels.go
+++ channels.go
@@ -1,19 +1,34 @@
 package main
 
 import "fmt"
+import "sync"
+
+var wg sync.WaitGroup
 
 func pass(c chan int, value int){
+	defer wg.Done()
 	c <- value*10 //Send
 }
 func main() {
-	channelA := make(chan int)
+	channelA := make(chan int,10)
 
-	go pass(channelA, 5)
-	go pass(channelA,30)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go pass(channelA,i)
+	}
+	wg.Wait()
+	close(channelA)
+
+	for item := range channelA {
+		fmt.Println(item)
+	}
+	
+	//go pass(channelA, 5)
+	//go pass(channelA,30)
 
 	//v1 := <-channelA  //Get
 	//v2 := <-channelA
 
-	v1, v2 := <- channelA, <- channelA
-	fmt.Println(v1,v2)
+	//v1, v2 := <- channelA, <- channelA
+	//fmt.Println(v1,v2)
 }
