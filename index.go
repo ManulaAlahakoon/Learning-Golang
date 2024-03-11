e560482f776734c130df1f6de03ac8fee667f0f5
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-11 16:28:30 +0530

    Update index.go

--- index.go
+++ index.go
@@ -31,40 +31,8 @@ type NewsMap struct {
 
 type NewsAggPage struct {
 	Title string
-	//News string
 	News map[string]NewsMap
 }
-
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-
-	news_map := make(map[string]NewsMap)
-
-    //// Replace if you want
-	channel := make(chan News, 1)
-	wg.Add(1)
-	go newsRoutine(channel)
-	wg.Wait()
-	close(channel)
-
-	for elem := range channel {
-	for idx, _ := range elem.Locations {
-		///Speep Mapping
-		//wg.Add(1)
-		//go speedMapping(news_map,elem,idx)
-		news_map[elem.Titles[idx]] = NewsMap{elem.Dates[idx], elem.Locations[idx]}
-		}
-	}
-		wg.Wait()
-	p := NewsAggPage{Title: "News aggregator", News: news_map}
-	t, _ := template.ParseFiles("basicTemplating.html")
-	t.Execute(w, p)
-
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Let's go</h1>")
-}
-
 func newsRoutine(c chan News) {
 	defer wg.Done()
 	var n News
@@ -75,13 +43,35 @@ func newsRoutine(c chan News) {
 
 	c <- n
 }
-/*
-func speedMapping(news_map map[string]NewsMap,elem News, idx int) {
-	defer wg.Done()
-	
-	news_map[elem.Titles[idx]] = NewsMap{elem.Dates[idx], elem.Locations[idx]}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+
+	news_map := make(map[string]NewsMap)
+
+	channel := make(chan News, 1)
+	wg.Add(1)
+	go newsRoutine(channel)
+	wg.Wait()
+	close(channel)
+
+	for elem := range channel {
+	for idx, _ := range elem.Locations {
+		news_map[elem.Titles[idx]] = NewsMap{elem.Dates[idx], elem.Locations[idx]}
+		}
+	}
+		
+	p := NewsAggPage{Title: "News aggregator", News: news_map}
+	t, _ := template.ParseFiles("basicTemplating.html")
+	t.Execute(w, p)
+
 }
-*/
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Let's go</h1>")
+}
+
+
+
 func main() {
 	//var s Sitemapindex
 	http.HandleFunc("/", indexHandler)
